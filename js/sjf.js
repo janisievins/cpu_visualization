c01a0b23767e0515edebf729d11b00f0183214e8
Jānis Ieviņš <Jnis Ievi>
2017-03-26 18:49:35 +0300

    Finish SJF. This time fo real!

--- js/sjf.js
+++ js/sjf.js
@@ -5,17 +5,17 @@ function submit_sjf(e){
 	var p1_burst = parseInt($('#p1_burst').val()),
 	p2_burst = parseInt($('#p2_burst').val()),
 	p3_burst = parseInt($('#p3_burst').val()),
-	// p4_burst = parseInt($('#p4_burst').val()),
-	// p5_burst = parseInt($('#p5_burst').val()),
-	burst_sum = p1_burst+p2_burst+p3_burst; //+p4_burst+p5_burst;
+	p4_burst = parseInt($('#p4_burst').val()),
+	p5_burst = parseInt($('#p5_burst').val()),
+	burst_sum = p1_burst+p2_burst+p3_burst+p4_burst+p5_burst;
 
 
 	var pList = [
 	    { id: 'P1', burst: p1_burst, start: 0, end: 0, className: 'style--p1'  },
 	    { id: 'P2', burst: p2_burst, start: 0, end: 0, className: 'style--p2'  },
 	    { id: 'P3', burst: p3_burst, start: 0, end: 0, className: 'style--p3'  },
-	    // { id: 'P4', val: p4_burst, start: null, end: null, className: 'style--p4'  },
-	    // { id: 'P5', val: p5_burst, start: null, end: null, className: 'style--p5'  },
+	    { id: 'P4', burst: p4_burst, start: 0, end: 0, className: 'style--p4'  },
+	    { id: 'P5', burst: p5_burst, start: 0, end: 0, className: 'style--p5'  },
 	];
 
 
@@ -42,41 +42,6 @@ function submit_sjf(e){
 
 	})
 
-	// console.log(data);
-
-
-	// console.log(pList);
-
-	// var p1_obj = {
-	// 	burst : p1_burst,
-	// 	start : 0,
-	// 	end   : p1_burst
-	// };
-
-	// var p2_obj = {
-	// 	burst : p2_burst,
-	// 	start : p1_obj.end,
-	// 	end   : p1_obj.end + p2_burst
-	// };
-
-	// var p3_obj = {
-	// 	burst : p3_burst,
-	// 	start : p2_obj.end,
-	// 	end   : p2_obj.end + p3_burst
-	// };
-
-	// var p4_obj = {
-	// 	burst : p4_burst,
-	// 	start : p3_obj.end,
-	// 	end   : p3_obj.end + p4_burst
-	// };
-
-	// var p5_obj = {
-	// 	burst : p5_burst,
-	// 	start : p4_obj.end,
-	// 	end   : p4_obj.end + p5_burst
-	// };
-
 	var options = {
 	    phases: [
 	      { start: 0, end: burst_sum, indicatorsEvery: 1, share: 1 },
@@ -85,22 +50,10 @@ function submit_sjf(e){
 	    fontSize: 16
 	  };
 
-  // var data_ex = [
-  //   [
-
-  //     { id: 'P1', start: p1_obj.start, end: p1_obj.end, className: 'style--p1'},
-  //     { id: 'P2', start: p2_obj.start, end: p2_obj.end, className: 'style--p2'},
-  //     { id: 'P3', start: p3_obj.start, end: p3_obj.end, className: 'style--p3'},
-
-  //   ]
-  // ];
-
-
   var data = [pList];
 
 
 	console.log(data);
-	// console.log(data_ex);
 
   var timeline = $('#timeline');
     timeline.show();
